3d214b9c995e6627ee03a41b99b5485a82351950
Semyon Gorin <1234snsks@gmail.com>
2026-04-25 21:10:15 +0300

    Shifted from online algorithm to buffer for input string.

--- m68k/base64_encoding.s
+++ m68k/base64_encoding.s
@@ -1,183 +1,196 @@
-.data
-buffer: .byte '_________________________________________________________________' ; [0; 0x40]
-in: .word 0x80
-out: .word 0x84
-mask: .word 0x3F
+    .data
+buffer:          .byte  '_________________________________________________________________' ; [0; 0x40]
+in:              .word  0x80
+out:             .word  0x84
+mask:            .word  0x3F
 
-.data
-.org 0x88
-lookup: .byte 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    .data
+.org             0x88
+lookup:          .byte  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+in_buffer:       .byte  '_________________________________________________________________' ; [0; 0x40]
 
-
-.text
+    .text
 _start:
-movea.l in, A0
-movea.l (A0), A0
-movea.l out, A1
-movea.l (A1), A1
+    movea.l  in, A0
+    movea.l  (A0), A0
+    movea.l  out, A1
+    movea.l  (A1), A1
 
-movea.l buffer, A2
+    movea.l  buffer, A2
+    movea.l  lookup, A3
+    movea.l  in_buffer, A4
 
-movea.l lookup, A3
+    ; This loop is ONLY needed because wrench gives 2000 ops limit.
+    ; Because of that I cant process input online, as my program for that needed ~2300 ops in the worst case scenario.
+in_to_buffer_loop:
+    cmp.l    0x40, D7
+    bge      overflow
+    add.l    1, D7
 
-; Process the 3 bytes in D0 into ascii symbols and add them to the buffer. 
-; D7 acts as loop counter.
+    move.b   (A0), D0
+    move.b   D0, (A4)+
+    cmp.b    0x0A, D0
+    beq      in_to_buffer_done
+
+    jmp      in_to_buffer_loop
+
+in_to_buffer_done:
+    move.b   0x00, D0
+    movea.l  in_buffer, A4
+
+    ; Process the 3 bytes in D0 into ascii symbols and add them to the buffer.
 loop:
 
-    ; In inner loop we load bytes from input in threes. 
+    ; In this inner loop we load bytes from input in threes.
     ; If we encounter '\n' we get out of the both loops, and add padding if needed.
     ; D6 acts as a loop counter.
-    move.l 0, D6
-    inner_loop_input:
+    move.l   0, D6
+inner_loop_input:
 
-        cmp.l 3, D6
-        bge inner_done_input;
+    cmp.l    3, D6
+    bge      inner_done_input
 
-        cmp.l 0x40, D7
-        bge overflow
-        add.l 1, D7
+    lsl.l    8, D0
+    move.b   (A4)+, D0
 
-        lsl.l 8, D0
-        move.b (A0), D0
+    ; if (c == '\n') {break;}
+    cmp.b    0x0A, D0
+    beq      done
 
-        ; if (c == '\n') {break;}
-        cmp.b 0x0A, D0
-        beq done
+    add.l    1, D6
+    jmp      inner_loop_input
 
-        add.l 1, D6
-        jmp inner_loop_input
+inner_done_input:
 
-        inner_done_input:
+    move.l   0, D6
+inner_loop_process:
+    cmp.l    4, D6
+    bge      inner_done_process
 
-    move.l 0, D6
-    inner_loop_process:
-        cmp.l 4, D6
-        bge inner_done_process
-
-        lsl.l 2, D0; Shift last 2 bits of the symbol out of the byte to the left
-        lsr.b 2, D0; Shift symbol inside the lowest byte to encode it
-        lsl.l 8, D2;
-        move.b D0, D1
-        move.b (A3, D1), D2;
-        
-
-        lsr.l 8, D0
-
-        add.l 1, D6
-        jmp inner_loop_process
-        inner_done_process:
-
-    move.l 0, D6
-    inner_loop_to_buffer:
-        cmp.l 4, D6
-        bge inner_done_to_buffer
-
-        move.b D2, (A2)+
-        lsr.l 8, D2
-
-        add.l 1, D6
-        jmp inner_loop_to_buffer
-        inner_done_to_buffer:
+    lsl.l    2, D0                           ; Shift last 2 bits of the symbol out of the byte to the left
+    lsr.b    2, D0                           ; Shift symbol inside the lowest byte to encode it
+    lsl.l    8, D2
+    move.b   D0, D1
+    move.b   (A3, D1), D2
 
 
-jmp loop
+    lsr.l    8, D0
+
+    add.l    1, D6
+    jmp      inner_loop_process
+inner_done_process:
+
+    move.l   0, D6
+inner_loop_to_buffer:
+    cmp.l    4, D6
+    bge      inner_done_to_buffer
+
+    move.b   D2, (A2)+
+    lsr.l    8, D2
+
+    add.l    1, D6
+    jmp      inner_loop_to_buffer
+inner_done_to_buffer:
+
+
+    jmp      loop
 done:
 
-; if we did not encounter overflow, the last byte will be '\n'. 
-; We have 3 options: '\n' was loaded 1st, 2nd or 3rd of 3. 
-; We need to insert 0, 2 or 1 padding chars, respectivly. 
-cmp.l 0, D6 ; if '\n' was loaded 1st of 3: 
-beq first
+    ; if we did not encounter overflow, the last byte will be '\n'.
+    ; We have 3 options: '\n' was loaded 1st, 2nd or 3rd of 3.
+    ; We need to insert 0, 2 or 1 padding chars, respectivly.
 
-cmp.l 1, D6 ; if '\n' was loaded 2nd of 3: 
-beq second
+    cmp.l    0, D6                           ; if '\n' was loaded 1st of 3:
+    beq      first
 
-cmp.l 2, D6 ; if '\n' was loaded 3rd of 3: 
-beq third
+    cmp.l    1, D6                           ; if '\n' was loaded 2nd of 3:
+    beq      second
+
+    cmp.l    2, D6                           ; if '\n' was loaded 3rd of 3:
+    beq      third
 
 
 exit:
-halt
+    halt
 
-; TODO: add processing of leftover symbols in D0.
 first:
-move.b 0x00, (A2)+
-jmp output
+    move.b   0x00, (A2)+
+    jmp      output
 
 second:
-lsr.l 8, D0 ; remove '\n' from the symbols.
+    lsr.l    8, D0                           ; remove '\n' from the symbols.
 
-lsl.l 6, D0
-lsr.b 2, D0
+    lsl.l    6, D0
+    lsr.b    2, D0
 
-move.b D0, D1
-move.b (A3, D1), D2
-lsl.l 8, D2
-lsr.l 8, D0
-move.b D0, D1
-move.b (A3, D1), D2
+    move.b   D0, D1
+    move.b   (A3, D1), D2
+    lsl.l    8, D2
+    lsr.l    8, D0
+    move.b   D0, D1
+    move.b   (A3, D1), D2
 
-move.b D2, (A2)+
-lsr.l 8, D2
-move.b D2, (A2)+
-lsr.l 8, D2
+    move.b   D2, (A2)+
+    lsr.l    8, D2
+    move.b   D2, (A2)+
+    lsr.l    8, D2
 
 
-move.b 0x3D, (A2)+
-move.b 0x3D, (A2)+
-move.b 0x00, (A2)+
-jmp output
+    move.b   0x3D, (A2)+
+    move.b   0x3D, (A2)+
+    move.b   0x00, (A2)+
+    jmp      output
 
 third:
-lsr.l 8, D0 ; remove '\n' from the symbols.
+    lsr.l    8, D0                           ; remove '\n' from the symbols.
 
-lsl.l 4, D0
-lsr.b 2, D0
+    lsl.l    4, D0
+    lsr.b    2, D0
 
-move.b D0, D1
-move.b (A3, D1), D2
+    move.b   D0, D1
+    move.b   (A3, D1), D2
 
-lsl.l 8, D2
-lsr.l 6, D0
-lsr.b 2, D0
+    lsl.l    8, D2
+    lsr.l    6, D0
+    lsr.b    2, D0
 
-move.b D0, D1
-move.b (A3, D1), D2
+    move.b   D0, D1
+    move.b   (A3, D1), D2
 
-lsl.l 8, D2
-lsr.l 6, D0
-lsr.b 2, D0
-move.b D0, D1
-move.b (A3, D1), D2
+    lsl.l    8, D2
+    lsr.l    6, D0
+    lsr.b    2, D0
+    move.b   D0, D1
+    move.b   (A3, D1), D2
 
-move.b D2, (A2)+
-lsr.l 8, D2
-move.b D2, (A2)+
-lsr.l 8, D2
-move.b D2, (A2)+
-lsr.l 8, D2
+    move.b   D2, (A2)+
+    lsr.l    8, D2
+    move.b   D2, (A2)+
+    lsr.l    8, D2
+    move.b   D2, (A2)+
+    lsr.l    8, D2
 
-move.b 0x3D, (A2)+
-move.b 0x00, (A2)+
-jmp output
+    move.b   0x3D, (A2)+
+    move.b   0x00, (A2)+
+    jmp      output
 
 output:
-movea.l buffer, A0
+    movea.l  buffer, A0
 output_loop:
-    move.b (A0)+, D0
+    move.b   (A0)+, D0
 
-    cmp.b 0x00, D0
-    beq exit
+    cmp.b    0x00, D0
+    beq      exit
 
-    move.b D0, (A1)    
+    move.b   D0, (A1)
 
-jmp output_loop
+    jmp      output_loop
 
 
 overflow:
-movea.l buffer, A2
-move.b 0x00, (A2)
-move.l 0xCCCCCCCC, (A1)
-jmp exit
+    movea.l  buffer, A2
+    move.b   0x00, (A2)
+    move.l   0xCCCCCCCC, (A1)
+    jmp      exit
 
 
